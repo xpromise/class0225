6066cc3e0248a1d16f5ad00cd99f1fdbae21e0e5
xiongjian <webjsforyou@163.com>
2019-06-10 09:48:35 +0800

    原生ajax post请求和其他

--- 05.nodejs/day07/01.ajax.js
+++ 05.nodejs/day07/01.ajax.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 // 使用内置中间件：暴露服务器的静态资源（只要在public文件夹下面文件，可以直接访问）
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/ajax', (req, res) => {
   console.log('服务器接收到请求了~');
   console.log(req.query);
 
   res.send('这是服务器返回的响应~');
+
+});
+
+app.post('/ajax', (req, res) => {
+  console.log('服务器接收到请求了~');
+  console.log(req.body);
+
+  res.json({name: 'jack', age: 20});
 
 });
 
