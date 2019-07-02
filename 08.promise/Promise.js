1dfa789386bc5311f4ccc98e718eea1a563cb662
xiongjian <webjsforyou@163.com>
2019-07-02 10:33:55 +0800

    promise的基本实现

--- 08.promise/Promise.js
+++ 08.promise/Promise.js
@@ -5,12 +5,58 @@
 }(this, (function () {
   'use strict';
 
-  function Promise() {
+  function Promise(executor) {
+    // 定义promise初始化状态
+    this.status = 'pending';
+    // 定义异步结果返回值
+    this.data = undefined;
+    // 定义回调函数数组
+    this.callbacks = [];
+    // 缓存this
+    const _self = this;
+
+    try {
+      // 立即调用executor，传入resolve，reject
+      executor(resolve, reject);
+    } catch (e) {
+      // 处理executor函数中throw异常。一旦异常，就改为失败状态
+      reject(e);
+    }
+
+    // 将promise对象改为成功的状态
+    function resolve(value) {
+      // promise对象状态只能有初始化变成成功/失败，不能由成功变成失败。
+      if (_self.status === 'pending') {
+        _self.status = 'fulfilled';
+        _self.data = value;
+
+        // 执行成功的回调函数 -> 必须异步执行
+        setTimeout(function () {
+          _self.callbacks.forEach((fnObj) => fnObj.onFulFilled(value))
+        }, 0)
+      }
+    }
+    
+    function reject(reason) {
+      if (_self.status === 'pending') {
+        _self.status = 'rejected';
+        _self.data = reason;
+
+        // 失败的回调函数 -> 必须异步执行
+        setTimeout(function () {
+          _self.callbacks.forEach((fnObj) => fnObj.onRejected(reason))
+        }, 0)
+      }
+    }
+    
 
   }
 
-  Promise.prototype.then = function () {
-
+  Promise.prototype.then = function (onFulFilled, onRejected) {
+    this.callbacks.push({
+      onFulFilled: onFulFilled,
+      onRejected: onRejected
+    })
   };
 
   Promise.prototype.catch = function () {
